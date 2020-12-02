fa8229392075a7edae5f950257040e591e777749
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2020-12-02 11:28:59 +0100

    feat(finally got a pattern to make RxRedisQueue never ending loops work properly)

--- node/test/queue_test.ts
+++ node/test/queue_test.ts
@@ -4,16 +4,12 @@ import * as rx from "rxjs";
 
 import * as rxo from "rxjs/operators";
 
-console.log(`
-
----------------------------
-
-Queue test, run independently to not interfere with Mocha. Launch with
-yarn queue-test
-
----------------------------
-
-`);
+/**
+ *
+ * This test module explores the right solution for never-ending queue loops to
+ * implement with RxRedisQueue.
+ *
+ */
 
 /**
  *
@@ -64,10 +60,21 @@ export class RedisMessageObjectExample implements IRedisMessageObject {
 
   }
 
-  // Here the class does something intense
+  /**
+   *
+   * Here the class does something intense. This is the logic to be processed at
+   * the switchMap of the loop. Errors here are allowed, see the switchMap.
+   *
+   */
   public somethingIntense$(): rx.Observable<number> {
 
-    if (this.a === 250) return rx.throwError(new Error("error at RedisMessageObjectExampler"));
+    // Will cut the connection, producing an very serious error, irrecuperable
+    // by the loop
+    if (this.a % 15 === 0) bRedis.close();
+
+    // Will throw an error every type a is divisible by 10, and will be handled
+    // by the switchMap graceously
+    if (this.a % 10 === 0) return rx.throwError(new Error("error at RedisMessageObjectExampler, perfectly handled by the switchMap without affecting the loop"));
 
     console.log("RedisMessageObjectExample doing something intense");
 
@@ -77,72 +84,95 @@ export class RedisMessageObjectExample implements IRedisMessageObject {
 
 }
 
-// // Will check some messages, close the connection, and fail
-// RxRedisQueue.loop$({
-//   redis: bRedis,
-//   keys: "q",
-//   constructorFunc: (params: any) => new RedisMessageObjectExample(params)
-// }).pipe(
-
-//   rxo.concatMap((o: any) => {
-
-//     return o.object.somethingIntense$();
-
-//   })
-
-// ).subscribe(
-
-//   (o: any) => console.log("D: next", o),
-
-//   (e: Error) => console.log("D: error", e.message),
-
-//   () => console.log("D: complete")
-
-// )
-
-RxRedisQueue.lget$({
+/**
+ *
+ * This is the pattern for an error resistant infinite loop.
+ *
+ */
+RxRedisQueue.get$({
   redis: bRedis,
-  keys: "q"
+  keys: [ "q0", "q1" ]
 }).pipe(
 
-  rxo.concatMap((o: any) => {
+  /**
+   *
+   * The switchMap will allow for an independent observable to be run. If this
+   * observable produces an error, it won't affect the main loop get$ one.
+   *
+   */
+  rxo.switchMap((o: any) => {
 
+    // An error at this point will cut the loop Observable, AVOID!!!
+    if (JSON.parse(o[1]).a % 7 === 0) throw new Error("error at loop produced by a bad error in the main switchMap that should be avoided");
+
+    console.log("D: retrieved from queue", o);
+
+    /**
+     *
+     * Here goes the logic of the message's processing. It has its own
+     * catchError independent from the main loop observable, so it will catch
+     * errors produced at the logic observable without crashing the loop get$
+     * one.
+     *
+     */
     return new RedisMessageObjectExample(JSON.parse(o[1])).somethingIntense$()
+    .pipe(
+
+      // This will reach the next without stoping the loop
+      rxo.catchError((o: Error) => rx.of(o.message))
+
+    )
 
   }),
 
+  // Don't do this either. Let the errors in the main loop observable flow and
+  // crash it, they are serious
+  // rxo.catchError((o: Error) => whatever),
+
+  // Ensures the repetition of the get$ main loop observable forever
   rxo.repeat()
+
+  // Do not use retry on the main loop. The main loop will fail for very serious
+  // reasons like losing the connection to the Redis, and should be handled
+  // seriously
+  // rxo.retry()
 
 ).subscribe(
 
+  /**
+   *
+   * Here, legitimate results from the logic observable will be processed, as
+   * well as any error processing made by its catchError.
+   *
+   */
   (o: any) => console.log("D: next", o),
 
-  (e: Error) => { console.log("D: error", e.message); process.exit(-1); },
+  /**
+   *
+   * Errors from the get$ loop will be processed here, terminating the loop. To restart the loop, another
+   */
+  (e: Error) => console.log("D: error", e.message),
 
   () => console.log("D: complete")
 
 )
 
-
 /**
  *
- * Post messages.
+ * Post messages every second, with increasing integers.
  *
  */
-rx.timer(10, 10)
+rx.timer(0, 100)
 .subscribe(
 
-  (o: number) => RxRedisQueue.set$(redis, "q",
-    new RedisMessageObjectExample({ a: o, b: `${o}` })).subscribe()
+  (o: number ) => rx.zip(
+
+    RxRedisQueue.set$(redis, "q0",
+      new RedisMessageObjectExample({ a: o, b: `${o}` })),
+
+    RxRedisQueue.set$(redis, "q1",
+      new RedisMessageObjectExample({ a: o, b: `${o}` }))
+
+  ).subscribe()
 
 )
-
-// RxRedisQueue.set$(redis, "q",
-//   new RedisMessageObjectExample({ a: 1, b: "1" })).subscribe();
-
-// RxRedisQueue.set$(redis, "q",
-//   new RedisMessageObjectExample({ a: 2, b: "2" })).subscribe();
-
-// RxRedisQueue.set$(redis, "q",
-//   new RedisMessageObjectExample({ a: 3, b: "3" })).subscribe();
-// }
